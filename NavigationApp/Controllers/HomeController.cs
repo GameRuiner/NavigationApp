0d3297c0a65ef01511d0724b55400744b7350636
GameRuiner <markgolovko@gmail.com>
2019-09-06 20:19:22 +0300

    Add Home/Filter

--- NavigationApp/Controllers/HomeController.cs
+++ NavigationApp/Controllers/HomeController.cs
@@ -17,6 +17,38 @@ namespace NavigationApp.Controllers
             return View(players.ToList());
         }
 
+        public ActionResult Filter(int? team, string position)
+        {
+            IQueryable<Player> players = db.Players.Include(p => p.Team);
+            if (team != null && team != 0)
+            {
+                players = players.Where(p => p.TeamId == team);
+            }
+            if (!String.IsNullOrEmpty(position) && !position.Equals("Все"))
+            {
+                players = players.Where(p => p.Position == position);
+            }
+
+            List<Team> teams = db.Teams.ToList();
+            // устанавливаем начальный элемент, который позволит выбрать всех
+            teams.Insert(0, new Team { Name = "Все", Id = 0 });
+
+            PlayersListViewModel plwm = new PlayersListViewModel
+            {
+                Players = players.ToList(),
+                Teams = new SelectList(teams, "Id", "Name"),
+                Positions = new SelectList(new List<string>()
+                {
+                    "Все",
+                    "Нападающий",
+                    "Полузащитник",
+                    "Защитник",
+                    "Вратарь"
+                })
+            };
+            return View(plwm);
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
---
+++ NavigationApp/Models/PlayersListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NavigationApp.Models
+{
+    public class PlayersListViewModel
+    {
+        public IEnumerable<Player> Players { get; set; }
+
+        public SelectList Teams { get; set; }
+
+        public SelectList Positions { get; set; }
+    }
+}
